44dac7023979b79571d2327ea00e65eb0e5c0edd
Yugain26Sharma <Yugain26Sharma@gmail.com>
2023-08-12 01:59:13 +0530

    removed test

--- src/test/java/com/flipkart/tests/SearchTest.java
+++ src/test/java/com/flipkart/tests/SearchTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,39 +20,22 @@ import com.flipkart.utility.SeleniumOperations;
 import com.jayway.jsonpath.JsonPath;
 
 public class SearchTest {
-	private WebDriver driver;
-	private BrowserSetup browserSetup;
-	private SeleniumOperations seOperations;
 
-	private HomePage homePage;
-	private SearchResultPage searchResultPage;
-
-	@BeforeMethod(groups  = { "sanity" })
-	public void setUp() {
-		browserSetup = new BrowserSetup();
-		driver = browserSetup.launchBrowser();
-		seOperations = new SeleniumOperations(driver);
-
-		homePage = new HomePage(driver);
-		searchResultPage = new SearchResultPage(driver);
+	
+	@Test(groups = { "sanity" })
+	public void validateInvalidSearchResult1() throws IOException {
+	Assert.assertEquals(false, true);	
 	}
 	
 	@Test(groups = { "sanity" })
-	public void validateInvalidSearchResult() throws IOException {
-		// fetch product deatils from json file
-		String json = JsonOperations.readJsonFile("testDataPath", "products.json");
-		String productCode = JsonPath.read(json, "$.invalidProduct.product1");
-
-		homePage.closeLoginPopUp();
-		homePage.searchProduct(productCode);
-
-		Boolean invalidSearch = searchResultPage.inavlidSearchResult();
-
-		Assertion.assertObjectEqual(true, invalidSearch, "Validate user get retry option for invalid search");
+	public void validateInvalidSearchResult2() throws IOException {
+		Assert.assertEquals(false, false);	
+	}
+	
+	@Test(groups = { "sanity" })
+	public void validateInvalidSearchResult3() throws IOException {
+		Assert.assertEquals(1, 1);	
 	}
 
-	@AfterClass
-	public void tearDown() throws InterruptedException {
-		BrowserSetup.closeBrowser();
-	}
+
 }
